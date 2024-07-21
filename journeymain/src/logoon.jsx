af598b72baefd9e78adae5443f4948f7369cacc7
CyberMan010 <mohamahasoun60@gmail.com>
2024-07-21 13:20:40 +0300

    Ramadan homepage and contact us

---
+++ journeymain/src/logoon.jsx
@@ -0,0 +1,17 @@
+import './swiper.css'
+
+function Swipping(){
+    return(
+        <container>
+            <img className="bg-transparent p-10 mt-96" src='./imgs/main-banner.jpg'/>
+            <img src='./imgs/big-Logo.png'/> 
+            
+            
+            
+            </container>
+            
+            
+       
+    )
+}
+export default Swipping;
